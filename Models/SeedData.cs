62c8f66b19c8a23282f127e7ba0ffc39c72008cf
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-09-19 09:54:41 +0100

    made changes for sqlite models and database template aldo adding gitignore file

---
+++ Models/SeedData.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CaesarMovie.Data;
+using System;
+using System.Linq;
+
+namespace CaesarMovie.Models
+public static class SeedData
+{
+
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+
+    }
+}
