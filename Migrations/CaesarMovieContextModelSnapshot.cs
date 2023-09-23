fd9ce1657e6f94a2747702bce866938ca580a026
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-09-23 21:05:24 +0100

    asp.net didnt add bin to gitignore so i am helping them

--- Migrations/CaesarMovieContextModelSnapshot.cs
+++ Migrations/CaesarMovieContextModelSnapshot.cs
@@ -27,6 +27,9 @@ namespace CaesarMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
