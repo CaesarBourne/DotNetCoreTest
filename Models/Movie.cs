51c0d802aaa88fc5e12b5668f4fbef0b392495eb
Emmanuel Ajiboye Adeleke <caesar@Emmanuels-MacBook-Pro.local>
2023-09-24 13:26:15 +0100

    completing base code for models validation implementing bDRY priciple

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -15,13 +15,17 @@ public class Movie
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    [Range(1, 100)]
-    [DataType(DataType.Currency)]
+
     [RegularExpression(@"^[A\A-Z]+[a-zA-Z\s]*$")]
     [Required]
     [StringLength(30)]
     public string? Genre { get; set; }
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public string? Rating { get; set; }
 }
